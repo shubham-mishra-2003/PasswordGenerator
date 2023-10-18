1766b19537c66d02b4f9f02e638f632015777ccf
shubham-mishra-2003 <shubhammishra3070@gmail.com>
2023-10-18 23:51:50 +0530

    Added reload functionality after copy the result

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Function to generate a random password based on user settings
 function generatePassword() {
   const passwordLength = document.getElementById("passwordlength").value;
   const uppercase = document.getElementById("uppercase").checked;
@@ -13,12 +12,9 @@ function generatePassword() {
     specialLetters
   );
   const password = generateRandomPassword(passwordLength, charset);
-
-  // Display the generated password in the input field
   document.getElementById("result").value = password;
 }
 
-// Function to generate a charset based on user preferences
 function generateCharset(uppercase, lowercase, numeric, specialLetters) {
   let charset = "";
   if (uppercase) charset += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -29,7 +25,6 @@ function generateCharset(uppercase, lowercase, numeric, specialLetters) {
   return charset;
 }
 
-// Function to generate a random password of a specified length
 function generateRandomPassword(length, charset) {
   let password = "";
   for (let i = 0; i < length; i++) {
@@ -39,24 +34,27 @@ function generateRandomPassword(length, charset) {
   return password;
 }
 
-// Event listener to generate a password when the "Generate" button is clicked
 document.getElementById("generate").addEventListener("click", generatePassword);
 
-// Initialize the password length display
 document
   .getElementById("passwordlength")
   .addEventListener("input", function () {
     document.querySelector(".PB-range-slidervalue").textContent = this.value;
   });
 
+
+
+
+  document.querySelector(".copy").addEventListener("click", copyToClipboard);
+
 // Function to copy the generated password to the clipboard
 function copyToClipboard() {
-  const passwordInput = document.getElementById("result");
-  passwordInput.select();
-  document.execCommand("copy");
-  const copyButton = document.querySelector(".copy .tooltip");
-  copyButton.textContent = copyButton.getAttribute("data-text-end");
-}
+  var resultField = document.getElementById("result");
 
-// Event listener to copy the password to the clipboard when the "Copy" button is clicked
-document.querySelector(".copy").addEventListener("click", copyToClipboard);
+  resultField.select();
+  document.execCommand("copy");
+
+  setTimeout(function () {
+    location.reload();
+  }, 2000);
+}
